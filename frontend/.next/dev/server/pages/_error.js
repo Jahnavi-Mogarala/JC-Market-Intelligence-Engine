99a5d4a89b4cbf381d93ecd6e3220d709eb042c2
Jahnavi-Mogarala <mogaralajahnavi9@gmail.com>
2026-05-02 20:22:37 +0530

    Configure GitHub Pages deployment via Actions and static export

---
+++ frontend/.next/dev/server/pages/_error.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
+R.c("server/chunks/ssr/0.nf_10x1f7p._.js")
+R.c("server/chunks/ssr/[externals]_next_dist_shared_lib_no-fallback-error_external_0fgm~bn.js")
+R.c("server/chunks/ssr/0.nf_06j7kkt._.js")
+R.c("server/chunks/ssr/[externals]__0.o~_~l._.js")
+R.c("server/chunks/ssr/0.nf_next_00_p.jj._.js")
+R.m("[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/dist/pages/_error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
+module.exports=R.m("[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/dist/pages/_error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/OneDrive/Documents/JC Hackstorm/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
---
+++ frontend/.next/dev/static/chunks/OneDrive_Documents_JC Hackstorm_frontend_pages__app_07xvfw~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0.nf_next_dist_compiled_0~15cs3._.js","static/chunks/0.nf_next_dist_shared_lib_0j4grnx._.js","static/chunks/0.nf_next_dist_client_0x5e14r._.js","static/chunks/0.nf_next_dist_0cgfpec._.js","static/chunks/0.nf_next_app_040lctt.js","static/chunks/[next]_entry_page-loader_ts_0-b-0e-._.js","static/chunks/0.nf_react-dom_0ulfauj._.js","static/chunks/0.nf_0qfzygk._.js","static/chunks/[root-of-the-server]__0jcrf8m._.js"],
+    source: "entry"
+});
